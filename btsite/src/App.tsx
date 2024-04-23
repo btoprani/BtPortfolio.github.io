36abe833e35f6846d614a404e3f8116b40953a5b
unknown <bhaven_toprani@yahoo.com>
2024-04-22 21:10:36 -0700

    Updated routing to make the app work

--- btsite/src/App.tsx
+++ btsite/src/App.tsx
@@ -7,7 +7,7 @@ import Contact from './pages/Contact';
 
 export default function App() {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename='/Portfolio'>
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/about' element={<About />} />
